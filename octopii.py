6e1938bd3c9c646539907c7d189bff9d9537c2f3
AcePhire <acephire27@gmail.com>
2025-10-20 15:17:41 +0300

    Added a Docker Image Build file

--- octopii.py
+++ octopii.py
@@ -24,7 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-output_file = "output.json"
+output_file = "/pii/output.json"
 notifyURL = ""
 
 import json, textract, sys, urllib, cv2, os, json, shutil, traceback
